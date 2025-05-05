837ca5cec2f0ef9f66a5e42599d4f1a79b7f68c5
Driely Santos <santosdriely98@gmail.com>
2025-05-05 15:44:22 -0300

    criação da seção about

---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const About = () =>{
+    return(
+        <section style={styles.sobre}>
+        <h2 style={styles.sobreTitulo}>Sobre nós</h2>
+        <p style={styles.sobreParagrafo}>Na Mecânica WC, oferecemos serviços de alta qualidade   para garantir que seu carro esteja sempre em ótimo estado. Nossa equipe é composta por profissionais experientes e dedicados. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum harum perferendis expedita odit optio iste at nulla voluptatibus repellendus dolor natus hic, quaerat ipsam fugit culpa itaque nihil placeat blanditiis.
+        </p>
+    </section>
+    )
+}
+
+const styles = {
+    sobre:{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        padding: "30px 15px",
+        backgroundColor: "#f9f9f9",
+    },
+    sobreTitulo:{
+        fontSize: "25px",
+        marginBottom: "10px",
+        color: "#4d4c4c",
+    },
+    sobreParagrafo: {
+        padding: "10px",
+        width: "100%",
+        maxWidth: "800px",
+        fontSize: "1rem",
+        margin: "0 auto",
+        textAlign: "justify",
+        color: "#555",
+        lineHeight: "1.5",
+
+    }
+}
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,54 +1,19 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Services from '../components/Services';
+import About from '../components/About';
 
 function Home() {
     return (
         <main>
             <Banner />
             <Services />
-
-            <section style={styles.sobre}>
-                <h2 style={styles.sobreTitulo}>Sobre nós</h2>
-                <p style={styles.sobreParagrafo}>Na Mecânica WC, oferecemos serviços de alta qualidade para garantir que seu carro esteja sempre em ótimo estado. Nossa equipe é composta por profissionais experientes e dedicados.</p>
-            </section>
+            <About />
+            
         </main>
 
     );
-  }
-  
-  const styles = {
-    servicos:{
-        padding: "30px 20px",
-        backgroundColor: "#fff"
-    },
-    servicosTitulo:{
-        color: "#4d4c4c",
-        fontSize: "22px",
-        marginBottom: "10px",
-    },
-    servicosLista:{
-        listStyle: "none",
-        padding: 0,
-        color: "#333",
-        fontSize: "16px",
-        lineHeight: "1.6",
-    },
-    sobre:{
-        padding: "30px 20px",
-        backgroundColor: "#f9f9f9",
-    },
-    sobreTitulo:{
-        fontSize: "22px",
-        marginBottom: "10px",
-    },
-    sobreParagrafo: {
-        fontSize: "16px",
-        color: "#555",
-        lineHeight: "1.5",
-    }
 }
-
   
 export default Home;
   
